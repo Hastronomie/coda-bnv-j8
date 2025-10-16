8fe27a7f59cc28aa93fd41ed76220f9a98ecca38
Mathilde <marco.mathilde@orange.fr>
2025-10-16 13:42:30 +0000

    ajout du script

---
+++ ex_3/dossiersDeBase.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "Nom du projet:"
+read projet
+mkdir $projet
+
+echo "#include <stdlib.h>
+#include <stdio.h>
+
+main()
+{
+    
+}" >> $projet/main.c
+
+echo "#include <stdlib.h>
+#include <stdio.h>
+
+()
+{
+	
+}">> $projet/$projet.c
+
+echo "#ifndef ___H__
+#define ___H__
+
+;
+
+#endif">> $projet/$projet.h
+
+echo "#Makefile
+NAME = 
+
+SRCS = main.c \
+	.c
+
+all: $(NAME)
+
+$(NAME): $(SRCS)
+	gcc $(SRCS) -o $(NAME)
+
+fclean:
+	rm -f $(NAME)
+
+re: fclean all">> $projet/Makefile
